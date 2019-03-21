6745229162667f0f6a2da99ead89461e06434de7
Riccardo <riccardo_london@live.it>
2019-03-21 13:11:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,6 +21,5 @@ WHERE category == 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT category, amount FROM pledges JOIN projects ON projects.id - pledges.project_id
-WHERE category = 'books' GROUP BY category"
+"SELECT category, WHERE category = 'books' GROUP BY category"
 end
