4b1373c3e3840b559ae99ed0ea75b3d2d5780bdc
Riccardo <riccardo_london@live.it>
2019-03-21 09:34:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,7 +12,7 @@ end
 # "SELECT name, SUM(amount) as 'diogo' FROM pledges JOIN users ON users.id = pledges.user_id GROUP BY name ORDER BY diogo, name"
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT name, CAST(amount as int) as 'amount' FROM pledges JOIN users ON users.id = pledges.user_id ORDER BY sum(amount), name"
+"SELECT name, amount FROM pledges JOIN users ON users.id = pledges.user_id ORDER BY amount, name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
